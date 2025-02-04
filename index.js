707adaad54800cfe280f2a21b761817e4e31ae97
leviballew <levi.ballew10@gmail.com>
2025-02-04 16:03:46 -0500

    checkbox is working with crossout

--- index.js
+++ index.js
@@ -10,18 +10,36 @@ function createTask() {
     return;
   };
 
-  const taskElement = document.createElement('li');
-  taskElement.textContent = taskInputText;
+  const taskItem = document.createElement('li');
+  taskItem.classList.add('task-item');
+  // taskItem.textContent = taskInputText;
+
+  const taskTextElement = document.createElement('span');
+  taskTextElement.classList.add('task-text');
+  taskTextElement.textContent = taskInputText;
+
+  const taskCheckbox = document.createElement('input');
+  taskCheckbox.type = 'checkbox';
+  taskCheckbox.classList.add('task-checkbox');
+  taskCheckbox.addEventListener('change', function() {
+    if (taskCheckbox.checked) {
+      taskTextElement.classList.add('completed');
+    } else {
+      taskTextElement.classList.remove('completed');
+    }
+  });
 
   const taskDeleteButton = document.createElement('button');
   taskDeleteButton.textContent = "Delete";
   taskDeleteButton.classList.add('delete-btn');
   taskDeleteButton.addEventListener('click', function() {
-    taskElement.remove();
+    taskItem.remove();
   });
 
-  taskElement.appendChild(taskDeleteButton);
-  taskList.appendChild(taskElement);
+  taskItem.appendChild(taskCheckbox);
+  taskItem.appendChild(taskTextElement);
+  taskItem.appendChild(taskDeleteButton);
+  taskList.appendChild(taskItem);
   taskInput.value = '';
 };
 
